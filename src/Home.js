26fdd698be46515598a4aab63946158a02574327
Jerry Frederic <jerry.fpl@gmail.com>
2021-03-14 05:38:07 +0100

    homepage-body-done

--- src/Home.js
+++ src/Home.js
@@ -12,15 +12,53 @@ function Home() {
           alt="amazon banner"
         />
         <div className="home__row">
-          <Product />
-          {/**product */}
+          <Product
+            id="766544"
+            title="The lean startup"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51CTIr1bJxL._SX325_BO1,204,203,200_.jpg"
+            rating={4}
+          />
+          <Product
+            id="988766"
+            title="New Apple Ipad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
+            price={598.99}
+            image="https://m.media-amazon.com/images/I/81lqq8UF5ML._AC_UY436_FMwebp_QL65_.jpg"
+            rating={5}
+          />
         </div>
         <div className="home__row">
-          {/**product */}
-          {/**product */}
-          {/**product */}
+          <Product
+            id="453432"
+            title="New Apple Watch Series 6 (GPS, 40mm) - Space Gray Aluminum Case with Black Sport Band"
+            price={399}
+            image="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MY9K2_VW_34FR+watch-44-stainless-gold-cell-6s_VW_34FR_WF_CO?wid=750&hei=712&trim=1,0&fmt=p-jpg&qlt=80&op_usm=0.5,0.5&.v=1599269261000,1599537309000"
+            rating={5}
+          />
+          <Product
+            id="343535"
+            title="Amazon Echo (3rd generation) Smart speaker with Alexa, Charcoal Fabric"
+            price={199}
+            image="https://m.media-amazon.com/images/I/41bdROAptkL.jpg"
+            rating={5}
+          />
+          <Product
+            id="323234"
+            title="Samsung Galaxy Watch 3 (45mm, GPS, Bluetooth) Smart Watch - Mystic Black (US Version)"
+            price={419}
+            image="https://images-na.ssl-images-amazon.com/images/I/81hgNpSWBiL._AC_SL1500_.jpg"
+            rating={5}
+          />
         </div>
-        <div className="home__row">{/**product */}</div>
+        <div className="home__row">
+          <Product
+            id="12232424"
+            title="SAMSUNG 65-inch Class The Frame TV with Customizable Black Bezel (2020 Model)"
+            price={1490}
+            image="https://images-na.ssl-images-amazon.com/images/I/81JLHGQk2hL._AC_SL1500_.jpg"
+            rating={5}
+          />
+        </div>
       </div>
     </div>
   );
--- src/Product.js
+++ src/Product.js
@@ -1,23 +1,24 @@
 import React from "react";
 import "./Product.css";
 
-function Product() {
+function Product({ title, image, price, rating }) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>The lean startup</p>
+        <p>{title}</p>
         <p className="product__price">
           <small>$</small>
-          <strong>29,99</strong>
+          <strong>{price}</strong>
         </p>
         <div className="product__rating">
-          <p>&#127775;</p>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>&#127775;</p>
+            ))}
         </div>
       </div>
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/51CTIr1bJxL._SX325_BO1,204,203,200_.jpg"
-        alt="the lean startup book image"
-      />
+      <img src={image} />
       <button>Add to basket</button>
     </div>
   );
