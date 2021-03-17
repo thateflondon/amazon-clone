b2f7f65be375ac87d9cf934e9c705dd5eefa7cc9
Jerry Frederic <jerry.fpl@gmail.com>
2021-03-17 17:10:24 +0100

    loginPage - Connecting to the BackEnd with Firebase

--- src/Login.js
+++ src/Login.js
@@ -6,6 +6,21 @@ import { useState } from "react";
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+
+  //Sign-In
+  const signIn = (e) => {
+    e.preventDefault();
+    //firebase login
+  };
+
+
+  //Register
+  const register = (e) => {
+    e.preventDefault();
+    //firebase register
+  };
+
   return (
     <div className="login">
       <Link to="/">
@@ -20,11 +35,25 @@ function Login() {
         <h1>Sign-In</h1>
         <form>
           <h5>E-mail</h5>
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
           <h5>Password</h5>
-          <input type="password" />
-          <button className="login__signInButton">Sign In</button>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            onClick={signIn}
+            type="submit"
+            className="login__signInButton"
+          >
+            Sign In
+          </button>
 
           <p>
             By signing-in you agree to the AMAZON CLONE Conditions of Use &
@@ -32,7 +61,7 @@ function Login() {
             Interest-Based Ads Notice
           </p>
 
-          <button className="login__registerButton">
+          <button onClick={register} className="login__registerButton">
             Create your Amazone Account
           </button>
         </form>
