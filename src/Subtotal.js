601333e2b45208d71f2d9995225d5396c94855d7
Jerry Frederic <jerry.fpl@gmail.com>
2021-03-19 17:44:08 +0100

    payment-page - initial commit

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -4,8 +4,10 @@ import CurrencyFormat from "react-currency-format";
 import { useStateValue } from "./StateProvider";
 import ShoppingBasket from "@material-ui/icons/ShoppingBasket";
 import { getBasketTotal } from "./reducer";
+import { useHistory } from "react-router-dom";
 
 function Subtotal() {
+  const history = useHistory();
   const [{ basket }, dispatch] = useStateValue();
   return (
     <div className="subtotal">
@@ -27,7 +29,9 @@ function Subtotal() {
         thousandSeparator={true}
         prefix={"$"}
       />
-      <button>Proceed to Checkout</button>
+      <button onClick={(e) => history.push("/payment")}>
+        Proceed to Checkout
+      </button>
     </div>
   );
 }
