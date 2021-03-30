3b66636e141621f9d4f5a81c47bd0b13944a28dd
Jerry Frederic <jerry.fpl@gmail.com>
2021-03-30 03:57:27 +0200

    payment page - firebase-tools debug OK -  functions emulators working - API created

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,25 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require('stripe')('sk_test_51IY6UlC1XM3tYf8dm54ukQBn6rtqDybh4lDADB3AON9hq6BPHvvx9cFoI4jp8tpn3IjAngCqvrKWxRxD9c6FMVht00fdVxFjUZ');
+
+
+//API
+
+//App config
+const app = express();
+
+//Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+//API routes
+app.get('/', (request, response) => response.status(200).send('hello world'));
+
+//Listen command
+exports.api = functions.https.onRequest(app);
+
+//http://localhost:5001/clone-71182/us-central1/api
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
